1a63bda8943507f7f7e592a4f289811e8ba950a3
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-11-13 20:42:06 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,7 +79,6 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
   movie.update(title: "Wat, huh?")
-  # __
 end
 
 def can_update_multiple_items_at_once
@@ -87,7 +86,7 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  __
+  Movie.update(title: "A Movie")
 end
 
 def can_destroy_a_single_item
